54f4d7dd6d99a9307670b26af28c2ee92cb312e6
Juraj Oršulić <juraj.orsulic@fer.hr>
2022-01-16 16:10:26 +0100

    Use rospy timer

--- scripts/mouse_position_publisher.py
+++ scripts/mouse_position_publisher.py
@@ -11,16 +11,16 @@ class MousePositionPublisher:
         self.position_pub = rospy.Publisher("mouse_position", Point, queue_size=1)
         self.position_msg = Point()
         self.mouse = Controller()
-        self.rate = rospy.Rate(10)
+
+    def publish_position(self, unused_event):
+        curr_position = self.mouse.position
+        self.position_msg.x = curr_position[0]
+        self.position_msg.y = curr_position[1]
+        self.position_pub.publish(self.position_msg)
 
     def run(self):
-        while not rospy.is_shutdown():
-            curr_position = self.mouse.position
-            self.position_msg.x = curr_position[0]
-            self.position_msg.y = curr_position[1]
-            self.position_pub.publish(self.position_msg)
-            self.rate.sleep()
-
+        rospy.Timer(rospy.Duration(1 / 10), self.publish_position)
+        rospy.spin()
 
 def main():
   rospy.init_node('mouse_position_publisher', anonymous=True)
