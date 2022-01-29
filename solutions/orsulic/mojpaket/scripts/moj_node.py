f21935477ce02a260469c3ef8511fbebb94738d3
Juraj Orsulic <juraj.orsulic@fer.hr>
2022-01-29 14:41:20 +0000

    Moje rjesenje

---
+++ solutions/orsulic/mojpaket/scripts/moj_node.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist 
+
+if __name__ == "__main__":
+    rospy.init_node('kontroler_kornjace')
+
+    rate = rospy.Rate(20)
+    publisher = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+
+    t0 = rospy.Time.now()
+
+    while not rospy.is_shutdown():
+        t = (rospy.Time.now() - t0).to_sec()
+
+        current_velocity = Twist()
+        current_velocity.linear.x = 2
+        current_velocity.angular.z = 0.8
+
+        publisher.publish(current_velocity)
+        rate.sleep()
